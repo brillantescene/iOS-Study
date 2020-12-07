004127901a1d2284bd3fc73fee3d0151a0577c97
yujin <glisteneugene@gmail.com>
2020-12-07 23:17:14 +0900

    tutorial

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -49,14 +49,18 @@ extension ViewController: UITextFieldDelegate {
     }
 }
 extension ViewController: UIImagePickerControllerDelegate {
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage else {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+        guard let selectedImage = info[.originalImage] as? UIImage else {
             fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
         }
+
         photoImageView.image = selectedImage
+
         dismiss(animated: true, completion: nil)
     }
 }
