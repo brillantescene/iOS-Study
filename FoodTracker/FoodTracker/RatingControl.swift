2f12bcc2aef8c1e8bec98ae8ebce4a7dee20e90e
yujin <glisteneugene@gmail.com>
2020-12-12 22:56:23 +0900

    stack array

--- FoodTracker/FoodTracker/RatingControl.swift
+++ FoodTracker/FoodTracker/RatingControl.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class RatingControl: UIStackView {
 
+    private var ratingButtons = [UIButton]()
+     
+    var rating = 0
+    
     override init(frame: CGRect){
         super.init(frame: frame)
         setupButtons()
@@ -28,19 +32,24 @@ class RatingControl: UIStackView {
     //MARK: Private Methods
     private func setupButtons() {
         
-        // Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        
-        // Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
             
-        
-        // Add the button to the stack
-        addArrangedSubview(button)
+            // Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
     }
 }
