b96c928c795b3b6b096c1c6e21532dfd2a5b72a1
yujin <glisteneugene@gmail.com>
2021-04-01 15:31:09 +0900

    이미지 설정

--- RadioButton/RadioButton/ViewController.swift
+++ RadioButton/RadioButton/ViewController.swift
@@ -11,30 +11,41 @@ class ViewController: UIViewController {
 
     @IBOutlet var radioButtons: [UIButton]!
     var indexOfOneAndOnlySelectedBtn: Int?
+    var check = 0
     
+    var whiteImages = ["decaffeine_w", "soybeanMilk_w", "oatMilk_w", "lactoseFreeMilk_w"]
+    var purpleImages = ["decaffeine_p", "soybeanMilk_p", "oatMilk_p", "lactoseFreeMilk_p"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        for index in radioButtons.indices {
+            radioButtons[index].setImage(UIImage(named: whiteImages[index]), for: .normal)
+            radioButtons[index].setImage(UIImage(named: purpleImages[index]), for: .selected)
+        }
     }
 
     @IBAction func touchButton(_ sender: UIButton) {
-        if let index = indexOfOneAndOnlySelectedBtn{
+        if let indexOfSelectedBtn = indexOfOneAndOnlySelectedBtn{
             if !sender.isSelected {
                 for unselectIndex in radioButtons.indices {
                     radioButtons[unselectIndex].isSelected = false
                 }
                 sender.isSelected = true
                 indexOfOneAndOnlySelectedBtn = radioButtons.firstIndex(of: sender)
+                check = sender.tag
             } else {
                 sender.isSelected = false
                 indexOfOneAndOnlySelectedBtn = nil
+                check = 0
             }
             
         } else {
             sender.isSelected = true
             indexOfOneAndOnlySelectedBtn = radioButtons.firstIndex(of: sender)
+            check = sender.tag
         }
-        print(sender.isSelected, indexOfOneAndOnlySelectedBtn ?? "nil")
+        print(sender.isSelected, indexOfOneAndOnlySelectedBtn ?? 0)
+        print("check: \(check)")
     }
 }
 
