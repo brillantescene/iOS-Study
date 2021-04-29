a899a70ff2f3a8e40c9ab59b85bf9d37b6f77be0
yujin <glisteneugene@gmail.com>
2021-04-29 20:23:56 +0900

    Stanford 5

---
+++ PlayimgCard/PlayimgCard/PlayingCard.swift
@@ -0,0 +1,24 @@
+//
+//  PlayingCard.swift
+//  PlayimgCard
+//
+//  Created by ✨EUGENE✨ on 2021/04/29.
+//
+
+import Foundation
+
+struct PlayingCard {
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit {
+        case spades
+        case hearts
+        case diamonds
+        case clubs
+    }
+    
+    enum Rank {
+        
+    }
+}
---
+++ PlayimgCard/PlayimgCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayimgCard
+//
+//  Created by ✨EUGENE✨ on 2021/04/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
