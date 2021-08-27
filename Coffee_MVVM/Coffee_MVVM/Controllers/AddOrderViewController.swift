bfa0c792006804d8da481b4c58dc1dec3b57fa32
yujin <glisteneugene@gmail.com>
2021-08-27 23:00:12 +0900

    order coffee MVVM

---
+++ Coffee_MVVM/Coffee_MVVM/Controllers/AddOrderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddOrderViewController.swift
+//  Coffee_MVVM
+//
+//  Created by ✨EUGENE✨ on 2021/08/27.
+//
+
+import UIKit
+
+class AddOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Coffee_MVVM/Coffee_MVVM/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  OrdersTableViewController.swift
+//  Coffee_MVVM
+//
+//  Created by ✨EUGENE✨ on 2021/08/27.
+//
+
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+}
