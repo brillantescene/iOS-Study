77ae8fac19050336752d8cf999bb77739a2209f6
yujin <glisteneugene@gmail.com>
2021-08-28 23:14:49 +0900

    web service

---
+++ Coffee_MVVM/Coffee_MVVM/Services/WebServices.swift
@@ -0,0 +1,26 @@
+//
+//  WebServices.swift
+//  Coffee_MVVM
+//
+//  Created by ✨EUGENE✨ on 2021/08/28.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case decodingError
+    case domainError
+    case urlError
+}
+struct Resource<T: Codable> { // Codable이 아니면 결과를 다시 매핑할 수 없음
+    let url: URL
+}
+
+class WebService {
+    
+    // 클로저가 탈출할 것이기 때문에 completion을 @escaping 클로저
+    // 성공 시 Result<T>를 얻고, 에러 시 NetworkError
+    func load<T>(resource: Resource<T>, completion: @escaping (Result<T, NetworkError>) -> Void {
+                 
+                 })
+}
