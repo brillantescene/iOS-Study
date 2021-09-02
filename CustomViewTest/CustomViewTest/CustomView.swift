ae653cfed9e5d795573abefb662d0307d155643b
yujin <glisteneugene@gmail.com>
2021-09-02 21:07:44 +0900

    xib 구성

---
+++ CustomViewTest/CustomViewTest/CustomView.swift
@@ -0,0 +1,12 @@
+//
+//  CustomView.swift
+//  CustomViewTest
+//
+//  Created by ✨EUGENE✨ on 2021/09/02.
+//
+
+import UIKit
+
+class CustomView: UIView {
+    
+}
---
+++ CustomViewTest/CustomViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomViewTest
+//
+//  Created by ✨EUGENE✨ on 2021/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
