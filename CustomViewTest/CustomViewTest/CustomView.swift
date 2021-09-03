7d98d077d7cdab9cba970891de639dd380a84ed4
yujin <glisteneugene@gmail.com>
2021-09-03 19:51:09 +0900

    window, view 공부 중

--- CustomViewTest/CustomViewTest/CustomView.swift
+++ CustomViewTest/CustomViewTest/CustomView.swift
@@ -9,4 +9,22 @@ import UIKit
 
 class CustomView: UIView {
     
+    @IBOutlet weak var testVIew: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        viewInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        viewInit()
+    }
+    
+    func viewInit() {
+        if let view = Bundle.main.loadNibNamed("CustomView", owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+             addSubview(view)
+        }
+    }
 }
--- CustomViewTest/CustomViewTest/ViewController.swift
+++ CustomViewTest/CustomViewTest/ViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var customVIewTest: CustomView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
