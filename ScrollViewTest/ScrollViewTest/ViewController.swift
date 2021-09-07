5279de37164615cf035cd5616bcc1bb98f3a0d3b
yujin <glisteneugene@gmail.com>
2021-09-07 22:29:49 +0900

    성공~ ㅂq

--- ScrollViewTest/ScrollViewTest/ViewController.swift
+++ ScrollViewTest/ScrollViewTest/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet var filterButtons: [UIButton]!
     
+    var cafeState = 0 // 어떤 옵션이 선택되어 있는지
+    var indexOfOneAndOnlyFilterButton: Int?
+    
     let filterTypes: [String] = ["decaffeine", "soybeanMilk", "oatMilk", "lactofree", "decaffeine", "soybeanMilk", "oatMilk", "lactofree"]
     
     override func viewDidLoad() {
@@ -43,22 +46,29 @@ class ViewController: UIViewController {
         }
         print("필터버튼 \(buttonWidth), \(buttonWidth/2)")
     }
-//    func setButton() {
-//        for i in 0..<filterTypes.count {
-//            let filterButton = UIButton()
-//            filterButton.setImage(UIImage(named: filterTypes[i]+"_w"), for: .normal)
-//            filterButton.setImage(UIImage(named: filterTypes[i]+"_p"), for: .selected)
-//            filterButton.contentMode = .scaleAspectFit //  사진의 비율을 맞춤.
-//            let xPosition = self.view.frame.width * CGFloat(i) // 현재 보이는 스크린에서 하나의 이미지만 보이게 하기 위해
-//
-//            filterButton.frame = CGRect(x: xPosition, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-//            // 즉 이미지 뷰가 화면 전체를 덮게 됨.
-//            scrollView.contentSize.width = self.view.frame.width * CGFloat(1+i)
-//            // 이미지의 사이즈를 파악하고 해당 이미지를 올릴 수 있는 만큼 scroll view의 크기를 늘린 후
-//            scrollView.addSubview(filterButton)
-//            // 이미지를 scroll view에 붙인다.
-//        }
-//    }
-
+    @IBAction func filterButtonDidTap(_ sender: UIButton) {
+        if indexOfOneAndOnlyFilterButton != nil{
+            if !sender.isSelected {
+                for unselectIndex in filterButtons.indices {
+                    filterButtons[unselectIndex].isSelected = false
+                }
+                sender.isSelected = true
+                indexOfOneAndOnlyFilterButton = filterButtons.firstIndex(of: sender)
+                cafeState = sender.tag
+            } else {
+                sender.isSelected = false
+                indexOfOneAndOnlyFilterButton = nil
+                cafeState = 0
+            }
+            
+        } else {
+            sender.isSelected = true
+            indexOfOneAndOnlyFilterButton = filterButtons.firstIndex(of: sender)
+            cafeState = sender.tag
+        }
+        print(sender.isSelected, indexOfOneAndOnlyFilterButton ?? 0)
+        print("check: \(cafeState)")
+//        serviceFunc()
+    }
 }
 
