13b5ad3cf31462be2ed6b4d774437c9daf96f1d2
yujin <glisteneugene@gmail.com>
2021-10-06 13:49:49 +0900

    subjects

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -2,6 +2,7 @@ import UIKit
 import RxSwift
 import Metal
 /*
+ //: # Observables
 // 유튜버
 let observable = Observable.just(1) // 1에 반응
 
@@ -48,7 +49,7 @@ observable2.subscribe { event in
 observable4.subscribe(onNext: { element in
     print(element)
 })
-*/
+
 
 // dispose
 let disposeBag = DisposeBag()
@@ -67,3 +68,40 @@ Observable<String>.create { observer in
     return Disposables.create()
 }.subscribe(onNext: {print($0)}, onError: {print($0)}, onCompleted: {print("Completed")}, onDisposed: {print("Disposed")})
     .disposed(by: disposeBag)
+
+
+//: # Subjects
+//: ## publish subjects
+
+// 기본적으로 Subject는 구독할 수 있고 이벤트를 방출할 수도 있음
+let disposeBag = DisposeBag()
+
+let subject = PublishSubject<String>() //String타입의 이벤트만 내보낼 수 있음
+
+// 이벤트 생성 (잡지 발행)
+subject.onNext("Issue 1")
+
+// 구독 생성 (잡지 구독자)
+subject.subscribe { event in
+    print(event)
+}
+
+// 이렇게 하면 실제로 아무일도 일어나지 않음. 이미 이벤트가 발생하고 나서 구독했기 때문
+// 이미 잡지 1호 나온 뒤 구독하면 1호에 대한 연락은 오지 않겠지. 2호 발행되고 나서 오지 않겠니?
+// 그래서 새 이벤트를 생성해보겠음
+
+subject.onNext("Issue 2")
+// 그럼 이제 next(Issue 2) 하고 출력됨
+subject.onNext("Issue 3")
+
+//: dipose 후 이벤트 생성하면 무시됨
+//subject.dispose()
+
+// 근데 onCompleted 하면
+subject.onCompleted()
+// completed 이벤트가 호출됨
+
+subject.onNext("Issue 4")
+ */
+
+//: ## Behavior subjects
