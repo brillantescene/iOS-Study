21e2b8f1530734ff9a682cf697e3a1479e8dfc8c
yujin <glisteneugene@gmail.com>
2021-10-07 14:37:16 +0900

    Subject 생성, Observable

--- CameraFilterRx/CameraFilterRx/PhotosCollectionViewController.swift
+++ CameraFilterRx/CameraFilterRx/PhotosCollectionViewController.swift
@@ -5,11 +5,22 @@
 //  Created by ✨EUGENE✨ on 2021/10/06.
 //
 
-import Foundation
 import UIKit
 import Photos
+import RxSwift
 
+/**
+ 기존에는 사진 선택하고 처음 페이지로 돌아갈 때 delegate 프로토콜 써야 하는데
+ Rx 쓰면 그럴 필요 업다 !
+ */
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    private let selectedPhotoSubject = PublishSubject<UIImage>()
+    
+    // 이걸로 다른 뷰컨에서 구독 가능
+    var selectedPhoto: Observable<UIImage> {
+        return selectedPhotoSubject.asObservable()
+    }
     
     private var images = [PHAsset]()
     
@@ -26,6 +37,22 @@ class PhotosCollectionViewController: UICollectionViewController {
         return self.images.count
     }
     
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedAsset = self.images[indexPath.row]
+        PHImageManager.default().requestImage(for: selectedAsset, targetSize: CGSize(width: 300, height: 300), contentMode: .aspectFit, options: nil) { [weak self] image, info in
+            
+            guard let info = info else { return }
+            
+            let isDegradedImage = info["PHImageResultIsDegradedKey"] as! Bool
+            
+            if !isDegradedImage {
+                if let image = image {
+                    self?.selectedPhotoSubject.onNext(image)
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+    }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as? PhotoCollectionViewCell else {
             fatalError("PhotoCollectionViewCell is not found")
