2c7f6074b01c3f5351c2e4f6c2f838b595d1b681
yujin <glisteneugene@gmail.com>
2021-10-13 13:04:08 +0900

    tag값

--- CalculatorRx/CalculatorRx/ViewController.swift
+++ CalculatorRx/CalculatorRx/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func touchUpNumber(_ sender: UIButton) {
+        print("tag \(sender.tag)")
+    }
+    
 }
 
