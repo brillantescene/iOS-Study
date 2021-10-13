32b28ebc3d8c0b6a2e3208ffbbb1280fabb24668
yujin <glisteneugene@gmail.com>
2021-10-13 14:13:58 +0900

    단항 연산자

--- CalculatorRx/CalculatorRx/ViewController.swift
+++ CalculatorRx/CalculatorRx/ViewController.swift
@@ -9,14 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dashboardLabel: UILabel!
+    var result: Double = 0
+    var operand1: Double = 0
+    var operand2: Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     
     @IBAction func touchUpNumber(_ sender: UIButton) {
         print("tag \(sender.tag)")
+        dashboardLabel.text = "\(sender.tag)"
+        result = Double(sender.tag)
+    }
+    
+    // 피연산자 2개 필요
+    @IBAction func touchUpOperator(_ sender: UIButton) {
+        
+        if sender.tag == 0 { // =
+            
+        }
+    }
+    
+    // 1개 필요
+    @IBAction func touchUpUnary(_ sender: UIButton) {
+        if sender.tag == 0 { // clear
+            result = 0
+            dashboardLabel.text = "\(result)"
+        } else if sender.tag == 1 { // sign
+            result = -result
+            dashboardLabel.text = "\(result)"
+        } else { // per
+            result = result / 100
+            dashboardLabel.text = "\(result)"
+        }
     }
     
 }
