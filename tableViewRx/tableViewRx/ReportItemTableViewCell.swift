f9cc883bbf4ad67f78fd22011d998781539ef5db
yujin <glisteneugene@gmail.com>
2021-10-21 13:02:46 +0900

    TableViewCell

---
+++ tableViewRx/tableViewRx/ReportItemTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReportItemTableViewCell.swift
+//  tableViewRx
+//
+//  Created by ✨EUGENE✨ on 2021/10/21.
+//
+
+import UIKit
+
+
+class ReportItemTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var address: UILabel!
+    
+}
