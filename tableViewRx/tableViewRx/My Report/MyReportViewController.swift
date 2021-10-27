0e30e069bd5b6e1abee44f3eeff92e9de63b4e96
yujin <glisteneugene@gmail.com>
2021-10-27 16:25:17 +0900

    좀 변경

--- tableViewRx/tableViewRx/My Report/MyReportViewController.swift
+++ tableViewRx/tableViewRx/My Report/MyReportViewController.swift
@@ -15,16 +15,24 @@ class MyReportViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var bag = DisposeBag()
+    
     typealias CitySectionModel = SectionModel<String, String>
     typealias CityDataSource = RxTableViewSectionedReloadDataSource<CitySectionModel>
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindTableView()
+    }
+    
     private func bindTableView() {
-        let firstCities = ["London", "Vienna", "Lisbon"]
-        let secondCities = ["Paris", "Madrid", "Seoul"]
+        let canceled = ["투썸", "스벅", "할리스"]
+        let ing = ["혜리의", "라떼는", "카페"]
+        let completed = ["캘리포니아", "도넛클럽", "올댓커피"]
         
         let sections = [
-            SectionModel<String, String>(model: "first section", items: firstCities),
-            SectionModel<String, String>(model: "second section", items: secondCities)
+            SectionModel<String, String>(model: "취소된 제보", items: canceled),
+            SectionModel<String, String>(model: "진행중인 제보", items: ing),
+            SectionModel<String, String>(model: "완료된 제보", items: completed)
         ]
         
         Observable.just(sections)
@@ -33,21 +41,22 @@ class MyReportViewController: UIViewController {
     }
     
     private var cityDatasource: CityDataSource {
-        let configureCell: (TableViewSectionedDataSource<CitySectionModel>, UITableView,IndexPath, String) -> UITableViewCell = { (datasource, tableView, indexPath,  element) in
+        let configureCell: (TableViewSectionedDataSource<CitySectionModel>, UITableView,IndexPath, String) -> UITableViewCell = { (datasource, tableView, indexPath, element) in
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath) as? NameCell else { return UITableViewCell() }
             cell.textLabel?.text = element
             return cell
         }
         
         let datasource = CityDataSource.init(configureCell: configureCell)
+        
+        datasource.canMoveRowAtIndexPath = { dataSource, indexPath in
+          return true
+        }
+        
         datasource.titleForHeaderInSection = { datasource, index in
             return datasource.sectionModels[index].model
         }
         
         return datasource
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
 }
