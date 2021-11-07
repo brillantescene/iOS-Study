7311b9a8647229b3011dd1a49a77d492ee4a7a18
yujin <glisteneugene@gmail.com>
2021-11-07 20:35:37 +0900

    one time code 완성

--- TextFieldTest/TextFieldTest/OneTimeCodeTextField.swift
+++ TextFieldTest/TextFieldTest/OneTimeCodeTextField.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 class OneTimeCodeTextField: UITextField {
     
+    var didEnterLastDigit: ((String) -> Void)?
+    
+    var defaultCharacter = "?"
     private var isConfigured = false // configure함수 한번만 호출하기 위함
 
     private var digitLabels = [UILabel]()
+    
+    // tap하면 키보드 올라옴
+    private lazy var tapRecognizer = UITapGestureRecognizer().then {
+        $0.addTarget(self, action: #selector(becomeFirstResponder))
+    }
     
     // 인증번호 자릿수
     func configure(with slotCount: Int = 6) {
@@ -19,6 +29,15 @@ class OneTimeCodeTextField: UITextField {
         isConfigured.toggle() // false -> true
         
         configureTextField()
+        
+        let labelsStackView = createLabelsStackView(with: slotCount)
+        addSubview(labelsStackView)
+        
+        addGestureRecognizer(tapRecognizer)
+        
+        labelsStackView.snp.makeConstraints {
+            $0.top.left.right.bottom.equalTo(self)
+        }
     }
     
     // textfield 설정
@@ -27,29 +46,59 @@ class OneTimeCodeTextField: UITextField {
         textColor = .clear
         keyboardType = .numberPad
         textContentType = .oneTimeCode
+        
+        addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        delegate = self
     }
     
     // 스택뷰
     private func createLabelsStackView(with count: Int) -> UIStackView {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false //autolayout
-        stackView.axis = .horizontal
-        stackView.alignment = .fill
-        stackView.distribution = .fillEqually
-        stackView.spacing = 8
-        
+        let stackView = UIStackView().then {
+            $0.axis = .horizontal
+            $0.alignment = .fill
+            $0.distribution = .fillEqually
+            $0.spacing = 8
+        }
         
         for _ in 1...count {
-            let label = UILabel()
-            label.translatesAutoresizingMaskIntoConstraints = false
-            label.textAlignment = .center
-            label.font = .systemFont(ofSize: 40)
-            label.backgroundColor = .blue
-            
+            let label = UILabel().then {
+                $0.textAlignment = .center
+                $0.font = .systemFont(ofSize: 40)
+                $0.isUserInteractionEnabled = true
+                $0.text = defaultCharacter
+            }
             stackView.addArrangedSubview(label)
-            
             digitLabels.append(label)
         }
         return stackView
+    }
+    
+    @objc
+    private func textDidChange() {
+        guard let text = self.text, text.count <= digitLabels.count else { return }
+        
+        for i in 0..<digitLabels.count {
+            let currentLabel = digitLabels[i]
+            
+            if i < text.count {
+                let index = text.index(text.startIndex, offsetBy: i)
+                currentLabel.text = String(text[index])
+            } else {
+                currentLabel.text = defaultCharacter
+            }
+        }
+        
+        if text.count == digitLabels.count {
+            didEnterLastDigit?(text)
+        }
+    }
+}
+
+extension OneTimeCodeTextField: UITextFieldDelegate {
+    
+    // 넘치는 숫자 처리
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let characterCount = textField.text?.count else { return false }
+        return characterCount < digitLabels.count || string == ""
     }
 }
--- TextFieldTest/TextFieldTest/ViewController.swift
+++ TextFieldTest/TextFieldTest/ViewController.swift
@@ -11,39 +11,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var emojiTextField: EmojiTextField!
     
-  
+    @IBOutlet weak var codeTextField: OneTimeCodeTextField!
     
-    var check = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        codeTextField.configure()
+        codeTextField.didEnterLastDigit = { [weak self] code in
+            print(code)
+        }
     }
 
 
 }
 
-extension ViewController: UITextFieldDelegate {
-    // 글자수제한
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        print(string)
-        if let char = string.cString(using: String.Encoding.utf8) {
-            let isBackSpace = strcmp(char, "\\b")
-            if isBackSpace == -92 {
-                return true
-            }
-        }
-        guard textField.text!.count < 1 else { return false }
-        check = true
-        return true
-    }
-    //
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if check == true {
-          second.becomeFirstResponder()
-        } else {
-            second.resignFirstResponder()
-        }
-        return true
-      }
-}
+//extension ViewController: UITextFieldDelegate {
+//}
