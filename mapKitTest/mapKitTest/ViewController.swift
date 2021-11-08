a547585b83ad0f51db24cb9eeb8cb89bdc649bc9
yujin <glisteneugene@gmail.com>
2021-11-08 13:08:46 +0900

    애플지도 테스트

---
+++ mapKitTest/mapKitTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  mapKitTest
+//
+//  Created by ✨EUGENE✨ on 2021/11/08.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
